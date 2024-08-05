4fcf2114c8872b6815022dacc5fe7d98717e001b
unknown <artemfronttend@gmail.com>
2024-08-05 19:48:10 +0300

    'ggggjj'

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,6 @@
 // Функция для генерации панели Dashboard с динамическим винрейтом и профитом
 function generateDashboard() {
-    fetch('http://localhost:3000/api/bets')
+    fetch('https://https://betsdiary.onrender.com/api/bets')
         .then(response => response.json())
         .then(data => {
             const winrate = calculateWinrate(data);
